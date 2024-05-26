9976c159f046d3b0c08e89b6717cb99ff0b7429b
Aman Pachouri <your-email-addressampachouri09@gmail.com>
2024-05-27 01:10:01 +0530

    replaced browserrouter with hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import AddItems from "./pages/AddItems";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="add-item" element={<AddItems />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
