09119eab5294247714014629c3ad2947e2c06345
Aman Pachouri <your-email-addressampachouri09@gmail.com>
2024-05-26 15:06:41 +0530

    Added AddItem page with FormField component

--- src/component/FormField.jsx
+++ src/component/FormField.jsx
@@ -1,6 +1,11 @@
-const FormField = () => {
+import React from 'react'
+
+const FormField = ({ id, label, type, name, value, onChange }) => {
     return (
-        <div>FormField</div>
+        <div className="flex flex-col gap-2">
+            <label className="text-xs font-semibold text-[#7E858E]" htmlFor={id}>{label}</label>
+            <input className="border h-10 border-[#D7DFE9] px-2" type={type} name={name} id={id} onChange={onChange} value={value} />
+        </div>
     );
 };
 
--- src/pages/AddItems.jsx
+++ src/pages/AddItems.jsx
@@ -1,7 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import FormField from '../component/FormField';
+import notification from '../utils/notification';
+
 function AddItems() {
+    const [formData, setFormData] = useState({
+        title: "Nickelson and Sons",
+        icon_url: "http://loremflickr.com/640/480",
+        link: "https://gaseous-pod.net",
+        description: "Eligendi cum eligendi nemo accusamus natus vero dolor.",
+        category: "Automotive",
+        tag: "request",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const notify = notification();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        notify("This is test", "SUCCESS");
+    }
+
     return (
-        <div >
-            Add Item
+        <div className="h-full flex flex-col md:flex-row">
+            <div className="md:w-1/2 flex flex-col">
+                <div className="mt-2 ml-7">
+                    <Link to="/" className="text-para text-xl">
+                        <span className="text-black font-semibold mr-2 text-3xl">&lt;</span>
+                        Users
+                    </Link>
+                </div>
+                <div className="flex-1 flex flex-col items-center py-6 px-6 md:px-72 gap-4">
+                    <h2 className="text-2xl font-normal font-rubik">Item Details</h2>
+                    <form onSubmit={handleSubmit} className="w-full flex flex-col gap-5">
+                        <FormField id="title" label="ITEM TITLE" type="text" name="title" value={formData.title} onChange={handleChange} />
+                        <FormField id="icon_url" label="ICON URL" type="text" name="icon_url" value={formData.icon_url} onChange={handleChange} />
+                        <FormField id="link" label="LINK" type="text" name="link" value={formData.link} onChange={handleChange} />
+                        <FormField id="description" label="DESCRIPTION" type="text" name="description" value={formData.description} onChange={handleChange} />
+                        <FormField id="category" label="CATEGORY" type="text" name="category" value={formData.category} onChange={handleChange} />
+                        <FormField id="tag" label="TAG" type="text" name="tag" value={formData.tag} onChange={handleChange} />
+                        <button className="bg-secnodaryButton text-white font-semibold text-sm py-2 mt-4">CREATE</button>
+                    </form>
+                </div>
+            </div>
+            <div className="h-full md:w-1/2 md:block hidden">
+                <img className="h-full w-full object-cover object-center" src="login.svg" alt="login" />
+            </div>
         </div>
     );
 }
